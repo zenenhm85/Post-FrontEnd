cedeb765a66837e76cd1fedcd77364ee8170ebab
Zenén Hernández Martínez <zenenhernandez85@gmail.com>
2020-07-22 16:42:50 +0100

    setting home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,151 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var jQuery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  ngOnInit() {
+    /*=============================================
+JD SLIDER
+=============================================*/
+
+    $('.slideshow').jdSlider({
+      wrap: '.slide-inner', //Especificar el slide que vamos a usar
+      isAuto: true, //Inicia la animación automáticamente
+      isLoop: true, //Al finalizar vuelve a comenzar
+      interval: 7000, //Tiempo por cada slide
+      isCursor: true, //Pausar animación con el mouse
+    });
+
+    /*=============================================
+PINTEREST GRID
+=============================================*/
+
+    $('.pinterest_grid').pinterest_grid({
+      no_columns: 4, //Número de columnas
+      padding_x: 10, //Márgenes internas horizontal
+      padding_y: 10, //Márgenes internas vertical
+      margin_bottom: 50, //Márgen externa inferor
+      single_column_breakpoint: 769, //Punto de quiebre para una sola columna
+    });
+
+    /*=============================================
+EKKO LIGHTBOX
+=============================================*/
+
+    $(document).on('click', "[data-toggle='lightbox']", function (e) {
+      e.preventDefault(); //Quitar eventos que vengan por defecto en el navegador
+      $(this).ekkoLightbox(); //Activar la acción del plugin Ekko Lightbox
+    });
+
+    /*=============================================
+MOUSE PARALLAX EFFECT - JQUERY
+=============================================*/
+
+    var mouseParallax = $('.mouseParallax');
+
+    $('.mouseParallax').mousemove(function (e) {
+      var posX = e.offsetX;
+      var posY = e.offsetY;
+
+      for (var i = 0; i < mouseParallax.length; i++) {
+        $(mouseParallax[i]).css({
+          width: '110%',
+          left: -posX / (i * 250) + '%',
+          top: posY / (i * 250) + '%',
+        });
+      }
+
+      $(mouseParallax[0]).css({ width: '100%' });
+    });
+
+    /*=============================================
+NAVEGACIÓN SCROLL
+=============================================*/
+
+    $('.nav-link').click(function (e) {
+      e.preventDefault();
+
+      var target = $(this).attr('href');
+
+      $('html, body').animate(
+        {
+          scrollTop: $(target).offset().top,
+        },
+        1000,
+        'easeOutBack'
+      );
+    });
+
+    /*=============================================
+SCROLL UP
+=============================================*/
+
+    $.scrollUp({
+      scrollText: '',
+      scrollSpeed: 2000,
+      easingType: 'easeOutQuint',
+    });
+    $('#scrollUp').css({
+      bottom: '20px',
+      right: '20px',
+      width: '50px',
+      height: '50px',
+      background: 'url(../assets/img/flecha.jpg)'
+    });
+    
+    /*=============================================
+STICKY JS
+=============================================*/
+
+    if (window.matchMedia('(min-width:992px)').matches) {
+      $('#inicio').sticky({ topSpacing: 0, zIndex: 1000 });
+    }
+
+    /*=============================================
+VALIDAR FORMULARIO
+=============================================*/
+
+    (function () {
+      'use strict';
+      window.addEventListener(
+        'load',
+        function () {
+          // Get the forms we want to add validation styles to
+          var forms = document.getElementsByClassName('needs-validation');
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener(
+              'submit',
+              function (event) {
+                if (form.checkValidity() === false) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+              },
+              false
+            );
+          });
+        },
+        false
+      );
+    })();
+
+    /*=============================================
+ICHECK
+=============================================*/
+
+    $('.icheck').iCheck({
+      checkboxClass: 'icheckbox_flat-blue',
+      radioClass: 'iradio_flat-blue',
+    });
+  }
 }
