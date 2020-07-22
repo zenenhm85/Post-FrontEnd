e4ea39275cb812adbeae30e8f430d6d88b339969
Zenén Hernández Martínez <zenenhernandez85@gmail.com>
2020-07-22 17:24:30 +0100

    modulating the home by components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { MouseComponent } from './mouse/mouse.component';
+import { ArticulosComponent } from './articulos/articulos.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SlideshowComponent,
+    GaleriaComponent,
+    MouseComponent,
+    ArticulosComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
